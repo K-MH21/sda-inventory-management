166667e5030a3fd3b52430ed473406c9c8e012a2
Kazero21 <Thinker7120@hotmail.com>
2024-09-01 14:36:14 +0300

    Add checks and conditions for Item class

--- Item.cs
+++ Item.cs
@@ -2,18 +2,57 @@ namespace InventoryManagement
 {
     public class Item
     {
-        private string name { set; get; }
-        private int quantity { set; get; }
-        private DateTime createdDate { set; get; }
+        private string _name = string.Empty;
+        private int _quantity;
+        private DateTime _createdDate;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                _name = value;
+            }
+        }
+        public int Quantity
+        {
+            get { return _quantity; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Quantity cannot be negative");
+                }
+                _quantity = value;
+            }
+        }
+
+        public DateTime CreatedDate
+        {
+            get { return _createdDate; }
+            set
+            { /*         Should I implement this condition?
+
+                 if (DateTime.Compare(value, DateTime.Now) == 1)
+                 {
+                     throw new ArgumentException("Date cannot be from the past");
+                 }
+                 */
+                _createdDate = value;
+            }
+        }
 
         public Item(string name, int quantity)
             : this(name, quantity, DateTime.Now) { }
 
         public Item(string name, int quantity, DateTime createdDate)
         {
-            this.name = name;
-            this.quantity = quantity;
-            this.createdDate = createdDate;
+            Name = name;
+            Quantity = quantity;
+            CreatedDate = createdDate;
         }
     }
 }
